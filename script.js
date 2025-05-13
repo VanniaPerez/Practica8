a6ea925706633555d605ddafce7c24ee0d0d09c9
VanniaPerez <Vperez1922@chih.conalep.edu.mx>
2025-05-13 07:46:50 -0600

    mi primer programa

---
+++ script.js
@@ -0,0 +1,36 @@
+function sumar(){
+    var num1;
+    num1=document.calc.txtnumero1.value;
+    var num2;
+    num2=document.calc.txtnumero2.value;
+    var opc;
+    opc=eval(num1) + eval(num2);
+    document.calc.resultado.value=opc;
+}
+function Resta(){
+    var num1;
+    num1=document.calc.txtnumero1.value;
+    var num2;
+    num2=document.calc.txtnumero2.value;
+    var opc1;
+    opc1=eval(num1) - eval(num2);
+    document.calc.resultado1.value=opc1;
+}
+function Multiplicacion(){
+    var num1;
+    num1=document.calc.txtnumero1.value;
+    var num2;
+    num2=document.calc.txtnumero2.value;
+    var opc2;
+    opc2=eval(num1) * eval(num2);
+    document.calc.resultado2.value=opc2;
+}
+function Divicion(){
+    var num1;
+    num1=document.calc.txtnumero1.value;
+    var num2;
+    num2=document.calc.txtnumero2.value;
+    var opc3;
+    opc3=eval(num1) / eval(num2);
+    document.calc.resultado3.value=opc3;
+}
